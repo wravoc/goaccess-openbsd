f91d4c23b373f8ac0bbfd744176e0308e5acff1c
quadhelion.engineering <erogravity@fastmail.com>
2023-05-28 16:35:02 -0700

    New Feature: Automatic ASN download and incorporation.

--- README.md
+++ README.md
@@ -20,6 +20,10 @@ OpenBSD has some quirks dealing with log formats so to make it easier I have mad
 
 * OpenBSD Themed
 
+* Automatic [ASN Database](https://www.arin.net/resources/guide/asn/) Download and Configuration Option
+
+  * Options for DB-IP, MaxMind Github 
+
 * IP Exclusions for [Uptime Robot](https://uptimerobot.com/?rid=d8e3c5122ea836) Web Monitoring 
 
 * Simplified shell script for single and combined log reporting output to HTML
@@ -41,9 +45,11 @@ OpenBSD has some quirks dealing with log formats so to make it easier I have mad
 ## Execute
 
 1. `chmod 750 goaccess.sh`
-2. `goaccess` (Uses OpenBSD theme, GeoIP, and Exclusions)
-3. `goaccess.sh` to create an HTML report for the current log *(no options used)*
-4. `goaccess.sh full` to combine all logs (including gzipped) into one report *(no options used)*
+2. `goaccess` (Uses OpenBSD theme, GeoIP, ASN, and Exclusions)
+    1. Downloads database to default `/var/db/GeoIP` installed by the OpenBSD binary
+
+3. `goaccess.sh` to create an HTML report for the current access.log *(ignores goaccess.conf)*
+4. `goaccess.sh full` to combine all logs (including gzipped) into one report *(ignore goaccess.conf)*
 5. Check your `/var/log`
     1. `goaccess-report-Month-Day.html`
     2. `goaccess-total-report.html`
@@ -54,11 +60,30 @@ OpenBSD has some quirks dealing with log formats so to make it easier I have mad
 
 After you've made your first reports you can choose to get more out of your logs and GoAccess
 
-1.  Inside the `server` section of your `httpd.conf` add the directive `log style combined`
-2.  `rcctl reload httpd`
-3.  Change shell script as needed. Add any [options](https://goaccess.io/man#options) for the shell script needed, some popular ones
-    * `--exclude-ip='216.144.248.23'`
-    * `--enable-geoip=<legacy|mmdb>`
+1. Add your ASN provider in the `CUSTOMIZE` section, does not permit authenticated logins
+
+2. Using with [MaxMind Data](https://www.maxmind.com/en/accounts/871098/geoip/downloads) on a free account 
+
+   1.  GeoIP **Country** Data: Copy database to `/var/db/GeoIP/GeoLite2-Country.mmdb`
+
+   2.  GeoIP **City** Data: Download database to `/var/db/GeoIP/GeoLite2-City.mmdb`
+
+       1.  Insert into `goasscess.conf` line 840 `geoip-database /var/db/GeoIP/GeoLite2-City.mmdb`
+
+   3.  **MaxMind ASN** Autoupdate requires this `ASN_URL` with your **license key added** to the URL
+
+       1.  ```
+           https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=YOUR_LICENSE_KEY&suffix=tar.gz
+           ```
+
+3. Inside the `server` section of your `httpd.conf` add the directive `log style combined`
+
+   1.  `rcctl reload httpd`
+
+4. Change shell script as needed. Add any [options](https://goaccess.io/man#options) for the shell script, some popular ones:
+   * `--exclude-ip='216.144.248.23'`
+   * `--unknowns-log=<filename>`
+   * `--invalid-requests=<filename>`
 
 ---
 
--- goaccess.sh
+++ goaccess.sh
@@ -7,69 +7,208 @@
 # elias@quadhelion.engineering
 ######################################
 
-version=$(pkg_info goaccess | head -n 1)
+######################################
+# License Attribution
+# ASN Data Provided by MaxMind
+# https:/www.maxmind.com
+# License CC BY 4.0
+######################################
 
+######################################
+# License Attribution
+# IP to ASN Data provided by DB-IP
+# https://db-ip.com/
+# License: CC BY 4.0
+######################################
+
+#######################################################################
+# CUSTOMIZE -----------------------------------------------------------
+# ---------------------------------------------------------------------
+ASN_URL="https://download.db-ip.com/free/dbip-asn-lite-2023-05.mmdb.gz"
+#
+# MaxMind GitHub 2021
+# "https://git.io/GeoLite2-ASN.mmdb"
+#
+# MaxMind Direct Updated (Requires Account)
+# "https://www.maxmind.com/en/accounts/871098/geoip/downloads"
+# CUSTOMIZE -----------------------------------------------------------
+#######################################################################
+
+
+ASN_FILE=$(echo ${ASN_URL} | sed 's:.*/::')
+version=$(pkg_info goaccess | head -n 1)
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+ASN_BASENAME=$(echo ${ASN_FILE%.*})
+FULL_REPORT_DATE=$(stat -f "%t%Sm" /var/log/goaccess-total-report.html | awk '{print $1,$2;}')
+CURRENT_MONTH_DAY=$(date '+%B %d')
+
+
+## Check to see if user running script can write to /var
+if id -nG "$USER" | grep -qw "wheel"; then
+	echo "\\n********************\033[38;5;75m User Mode \033[0;0m************************"
+	echo "Permissions are correct to write to /var/"
+	echo "*******************************************************\\n"
+else
+	echo  "${USER} \033[38;5;9mdoes not have permission to write to /var/\033[0;0m"
+	echo "*******************************************************\\n"
+fi
+
+
+## Script Usage
 info()
 {
    echo
    echo "********************\033[38;5;75m Base Mode \033[0;0m************************"
    echo "${version}"
-   echo "*******************************************************"
    echo "Available mode: \033[38;5;208mfull\033[0;0m"
    echo "Argument full will combine all access.log.*.gz"
+   echo "Arguement full will use all GeoIP Databases"
    echo "*******************************************************\\n"
 }
 
+## If ASN DB doesn't exist, ask user to download it 
+if [ ! -f "/var/db/GeoIP/ASN.mmdb" ] ; then
+    echo
+	echo "\033[38;5;208mWould you like to download ASN Data to /var/db/GeoIP/GeoLite2-Country.mmdb\033[0;0m? (y/N)"
+	read answer
+	case $answer in
+		y|Y|"")
+			echo "*******************************************************"
+			echo "\033[38;5;75mDownloading ASN Data \033[0;0m"
+			echo "Destination: /var/db/GeoIP/GeoLite2-Country.mmdb"
+			echo "*******************************************************"
+			wget --header='Accept-Encoding: gzip' --tries=3 ${ASN_URL} && gunzip ${ASN_FILE}
+			echo "****************\033[38;5;75m Unzipped, Moving File \033[0;0m****************"
+			mv ${DIR}/${ASN_BASENAME} /var/db/GeoIP/ASN.mmdb
+			echo 
+			echo
+			echo "*********************\033[38;5;76m Success \033[0;0m*************************\\n"
+			;;
+		no|n)
+		    echo
+			echo "*******************************************************"
+			echo "\033[38;5;75mSkipping ASN Database \033[0;0m"
+			echo "*******************************************************"
+			;;
+	esac
+fi
+
+
+## Check for ASN Install
+if [ -f "/var/db/GeoIP/ASN.mmdb" ] ; then
+	ASN_INSTALLED=1
+else
+	ASN_INSTALLED=0
+fi
+	
+
+## Get script argument "full" or not
 if [ $# -ne 1 ] ; then
    info
    
-   date=`date +%b%e`
-   filename=goaccess-report-${date}.html
+   
+## Set report name with current date
+date=`date +%b%e`
+filename=goaccess-report-${date}.html
 
-   echo
-   echo "********************\033[38;5;75m Results \033[0;0m**************************"   
-   zcat -f /var/www/logs/access.log \
-    | grep -v 'logfile turned over$' \
-    | awk '$8=$1$8' \
-    | goaccess \
-        --no-global-config \
-        --log-format='%v %h %^ %e [%d:%t %^] "%r" %s %b "%R" "%u" %^' \
-        --date-format='%d/%b/%Y' \
-        --time-format='%T' \
-        --output='/var/log/'${filename}
-echo "*******************************************************"
 
-if [ -e /var/log/${filename} ]
-   then
-      echo "Report created at /var/log/${filename}"
-      echo "*******************************************************\\n"
-   else
-      echo "Error creating /var/log/${filename}"
-      echo "*******************************************************\\n"
-fi
-
-else
-   echo
-   echo "********************\033[38;5;75m Results \033[0;0m**************************"
-   echo
-   zcat -f /var/www/logs/access.log* \
-    | grep -v 'logfile turned over$' \
-    | awk '$8=$1$8' \
-    | goaccess \
-        --no-global-config \
-        --log-format='%v %h %^ %e [%d:%t %^] "%r" %s %b "%R" "%u" %^' \
-        --date-format='%d/%b/%Y' \
-        --time-format='%T' \
-        --output='/var/log/goaccess-total-report.html'
+## Run correct report for just the current /var/www/logs/access.log
+case $ASN_INSTALLED in
+   1)
+   	  echo
+	  echo "*********************\033[38;5;75m Results \033[0;0m*************************"   
+	  zcat -f /var/www/logs/access.log \
+	   | grep -v 'logfile turned over$' \
+	   | awk '$8=$1$8' \
+	   | goaccess \
+		   --no-global-config \
+		   --ignore-crawlers \
+		   --log-format='%v %h %^ %e [%d:%t %^] "%r" %s %b "%R" "%u" %^' \
+		   --date-format='%d/%b/%Y' \
+		   --time-format='%T' \
+		   --geoip-database=/var/db/GeoIP/ASN.mmdb \
+		   --geoip-database=/var/db/GeoIP/GeoLite2-Country.mmdb \
+		   --output='/var/log/'${filename}
    echo "*******************************************************"
+   ;;
+   0)
+   	  echo
+   	  echo "*********************\033[38;5;75m Results \033[0;0m*************************"   
+   	  zcat -f /var/www/logs/access.log \
+    	| grep -v 'logfile turned over$' \
+    	| awk '$8=$1$8' \
+    	| goaccess \
+        	--no-global-config \
+			--ignore-crawlers \
+        	--log-format='%v %h %^ %e [%d:%t %^] "%r" %s %b "%R" "%u" %^' \
+        	--date-format='%d/%b/%Y' \
+        	--time-format='%T' \
+			--geoip-database=/var/db/GeoIP/GeoLite2-Country.mmdb \
+        	--output='/var/log/'${filename}
+			echo "*******************************************************"
+	;;
+	esac
 
-if [ -e /var/log/goaccess-total-report.html ]
-   then
-      echo "Report created at /var/log/goaccess-total-report.html"
-      echo "*******************************************************\\n"
-   else
-      echo "Error creating /var/log/goaccess.total-report.html"
-      echo "*******************************************************\\n"
-fi
+	if [ -e /var/log/${filename} ]
+   		then
+		    echo
+		    echo "*********************\033[38;5;76m Success \033[0;0m*************************"
+      		echo "Report created at /var/log/${filename}"
+      		echo "*******************************************************\\n"
+   		else
+      		echo "Error creating /var/log/${filename}"
+      		echo "*******************************************************\\n"
+	fi
+
+
+## Run report for all web logs, include the gzipped logs with ASN data
+else
+ case $ASN_INSTALLED in
+   1)
+   	  echo
+   	  echo "*********************\033[38;5;75m Results \033[0;0m*************************"
+   	  echo
+   	  zcat -f /var/www/logs/access.log* \
+    	| grep -v 'logfile turned over$' \
+    	| awk '$8=$1$8' \
+    	| goaccess \
+        	--no-global-config \
+			--ignore-crawlers \
+        	--log-format='%v %h %^ %e [%d:%t %^] "%r" %s %b "%R" "%u" %^' \
+        	--date-format='%d/%b/%Y' \
+        	--time-format='%T' \
+			--geoip-database=/var/db/GeoIP/ASN.mmdb \
+			--geoip-database=/var/db/GeoIP/GeoLite2-Country.mmdb \
+        	--output='/var/log/goaccess-total-report.html'
+	;;
+	0)	
+	   echo
+	   echo "*********************\033[38;5;75m Results \033[0;0m*************************"
+	   echo
+	   zcat -f /var/www/logs/access.log* \
+		| grep -v 'logfile turned over$' \
+		| awk '$8=$1$8' \
+		| goaccess \
+			--no-global-config \
+			--ignore-crawlers \
+			--log-format='%v %h %^ %e [%d:%t %^] "%r" %s %b "%R" "%u" %^' \
+			--date-format='%d/%b/%Y' \
+			--time-format='%T' \
+			--geoip-database=/var/db/GeoIP/GeoLite2-Country.mmdb \
+			--output='/var/log/goaccess-total-report.html'
+	;;
+  esac	
+  
+
+	if [ -e /var/log/goaccess-total-report.html ] || [ FULL_REPORT_DATE == CURRENT_MONTH_DAY ]
+   	then
+	    echo
+	    echo "*********************\033[38;5;76m Success \033[0;0m*************************"
+      	echo "Report created at /var/log/goaccess-total-report.html"
+      	echo "*******************************************************\\n"
+   	else
+      	echo "Error creating /var/log/goaccess.total-report.html"
+      	echo "*******************************************************\\n"
+	fi
 
 fi
